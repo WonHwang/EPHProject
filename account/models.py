0bba509801ccaccb9c473497f67ad0bfba16d2b1
Jiwon Hwang <ediblepotato@gmail.com>
2023-06-25 20:47:43 +0900

    chore: use email as username

--- account/models.py
+++ account/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     
+    email = models.EmailField(unique=True, null=False)
     birthYMD = models.CharField(max_length=6)
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
